b8eade83d03ec15076feda8c60e94f3166b57731
Khaled Hassan <132917415+KhaledHassangit@users.noreply.github.com>
2024-09-09 19:11:20 +0300

    dev tools removed

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -12,16 +12,22 @@
 // export default LaylStore 
 
 import { legacy_createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'; // No need to destructure
 import RootReducer from './RootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 const Middleware = [thunk];
 
-const LaylStore = legacy_createStore(
-    RootReducer,
-    initialState,
-    applyMiddleware(...Middleware) 
-    );
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools
+    : (f) => f;  
 
-    export default LaylStore;
+const LaylStore = legacy_createStore(
+  RootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...Middleware))
+);
+
+export default LaylStore;
